0a0f0ded242adbb26687e56c6094e68d1eaa73db
Joshua Lee <joshualee@iconfigure.org>
2017-01-12 20:34:46 -1000

    setting up server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const { Server : WebSocketServer } = require('ws');
+const server = require('http').createServer();
+const wss = new WebSocketServer({ server });
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/hello', (req, res) => {
+  const hello = 'world';
+  res.json({ hello });
+});
+
+wss.on('connection', client => {
+
+  client.on('message', message  => {
+    console.log('received: %s', message);
+  });
+
+  client.send('connected to ws server');
+});
+
+server.on('request', app);
+
+server.listen(PORT, () => {
+  console.log('server listening on' )
+})
